bb0b0aa0e33495b0188e2b94969e175b5d0db978
psyomn <lethaljellybean@gmail.com>
2012-11-01 01:57:34 -0400

    First commit

---
+++ app/models/song.rb
@@ -0,0 +1,3 @@
+class Song < ActiveRecord::Base
+  attr_accessible :description, :name
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sunbox::Application.config.secret_token = '66b9d4f197cf569322747a3471b6e2682b5bb16088a4dcc57f54def91a0afb73b002a2e26c52f030e0d4154bad661aca36d69da233f0dccb942559f7a78b600a'
