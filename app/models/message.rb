c46641ae2e1682af0ba419df482b555243eed597
psyomn <lethaljellybean@gmail.com>
2013-01-18 17:27:11 -0500

    message.rb: fixed relations from_user, to_user

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,7 @@
 class Message < ActiveRecord::Base
   attr_accessible :contents, :from_user_id, :to_user, :to_user_id
+
+  belongs_to :from_user, :foreign_key => :from_user_id, :class_name => "User"
+  belongs_to :to_user, :foreign_key => :to_user_id, :class_name => "User"
+
 end
