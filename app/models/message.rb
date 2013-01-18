4eceae9b19d73a11efd638e4c6ad2d683567fa28
psyomn <lethaljellybean@gmail.com>
2013-01-18 18:20:46 -0500

    message.rb: broke down a line that was too long. derp derp.

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,9 @@
 class Message < ActiveRecord::Base
-  attr_accessible :contents, :from_user_id, :to_user, :to_user_id
+  attr_accessible :contents, 
+    :from_user_id, 
+    :to_user, 
+    :to_user_id, 
+    :subject
 
   belongs_to :from_user, :foreign_key => :from_user_id, :class_name => "User"
   belongs_to :to_user, :foreign_key => :to_user_id, :class_name => "User"
