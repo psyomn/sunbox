2911f9423d20e145e91cd498db0a526d09b0d37d
psyomn <lethaljellybean@gmail.com>
2013-01-19 00:31:59 -0500

    message.rb small changes in model

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,11 +1,12 @@
 class Message < ActiveRecord::Base
   attr_accessible :contents, 
     :from_user_id, 
-    :to_user, 
     :to_user_id, 
     :subject
 
   belongs_to :from_user, :foreign_key => :from_user_id, :class_name => "User"
   belongs_to :to_user, :foreign_key => :to_user_id, :class_name => "User"
+
+  has_one :next_message, :foreign_key => :next_message_id, :class_name => "Message"
 
 end
