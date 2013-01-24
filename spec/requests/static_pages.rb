1bc5dfdda552944b33f6cf49989d37c46af797a1
psyomn <lethaljellybean@gmail.com>
2013-01-24 01:51:27 -0500

    rspec/staticpages: more tests for the rest of pages.

--- spec/requests/static_pages.rb
+++ spec/requests/static_pages.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 
+# Test all the static pages.
+
 describe "Static pages" do 
   describe "Home" do
     it "should have the content" do 
@@ -7,4 +9,18 @@ describe "Static pages" do
       page.should have_content('Sunbox')
     end 
   end
+
+  describe "About" do 
+    it "should have the contents of the about page" do
+      visit '/about'
+      page.should have_content('Sunbox hopes to be a platform for people to share, learn and collaborate on sunvox songs.')
+    end 
+  end
+
+  describe "Contact" do
+    it "should state opensource-ness and location to get source" do 
+      visit '/contact'
+      page.should have_content('github.com/psyomn/sunbox.git')
+    end 
+  end 
 end 
