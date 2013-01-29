6ad123709199e2c41e0afe2febe198fe6f5d93e7
psyomn <lethaljellybean@gmail.com>
2013-01-28 22:47:24 -0500

    static_page_controller.rb: show latest news (not oldest herp derp).

--- app/controllers/static_page_controller.rb
+++ app/controllers/static_page_controller.rb
@@ -2,11 +2,10 @@ class StaticPageController < ApplicationController
   def home
     @users = User.find(:all, :limit => 10, :order => "created_at desc")
     @songs = Song.find(:all, :limit => 10, :order => "created_at desc")
-    @news_item = Newspaper.first
+    @news_item = Newspaper.last
 
     @news_item = Newspaper.new(:title => "No News"\
     , :text => "No news is good news. More to come soon.") if @news_item.nil?
-
   end
 
   def about
