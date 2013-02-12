e566c89eef2a518a39224aff6199c920712b1c15
psyomn <lethaljellybean@gmail.com>
2013-02-11 23:54:25 -0500

    models/collaboration.rb: Adding validators

--- app/models/collaboration.rb
+++ app/models/collaboration.rb
@@ -1,3 +1,8 @@
 class Collaboration < ActiveRecord::Base
   attr_accessible :description, :owner_id
+
+  # Validations 
+  validates :owner_id, :presence => true 
+  validates_length_of :description, :minimum => 10
+
 end
