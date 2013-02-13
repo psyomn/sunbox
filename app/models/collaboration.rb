4cfb47ca468ed49df4a809e5e28df72d94c2fd56
psyomn <lethaljellybean@gmail.com>
2013-02-12 23:08:54 -0500

    Tidy collaboration views, models.

--- app/models/collaboration.rb
+++ app/models/collaboration.rb
@@ -1,6 +1,9 @@
 class Collaboration < ActiveRecord::Base
   attr_accessible :description, :owner_id
 
+  belongs_to :owner, :foreign_key => :owner_id, :class_name => "User"
+  has_many :posts, :through => :collaboration_posts
+
   # Validations 
   validates :owner_id, :presence => true 
   validates_length_of :description, :minimum => 10
--- app/models/collaboration_post.rb
+++ app/models/collaboration_post.rb
@@ -1,3 +1,6 @@
 class CollaborationPost < ActiveRecord::Base
   attr_accessible :author_id, :collaboration_id, :message
+
+  belongs_to :collaboration
+  belongs_to :author, :foreign_key => :author_id, :class_name => "User"
 end
